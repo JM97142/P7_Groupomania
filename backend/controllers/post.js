f48d148ea7cb029824ba3ecb0f0e6d22e6e01303
JM97142 <jeanmichel.taret@gmail.com>
2022-01-09 17:54:51 +0100

    Maj controllers/middlewares/routes pour la méthode post

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -35,3 +35,145 @@ exports.newPost = (req, res, next) => {
     });
     connection.end();
 };
+
+// Récupération de tous les posts
+exports.getAllPosts = (req, res, next) => {
+    const connection = database.connect();
+  
+    const sql =
+        "SELECT Posts.id AS postId, Posts.publication_date AS postDate, Posts.imageurl AS postImage, Posts.content as postContent, Users.id AS userId, Users.name AS userName, Users.pictureurl AS userPicture\
+        FROM Posts\
+        INNER JOIN Users ON Posts.user_id = Users.id\
+        ORDER BY postDate DESC";
+    connection.execute(sql, (error, rawPosts, fields) => {
+        if (error) {
+            connection.end();
+            res.status(500).json({ error: error.sqlMessage });
+        } else {
+            this.getCommentsOfEachPosts(rawPosts, connection)
+            .then((postsWithoutLikes) => {
+            const cryptedCookie = new Cookies(req, res).get("snToken");
+            const userId = JSON.parse(
+                cryptojs.AES.decrypt(
+                cryptedCookie,
+                process.env.COOKIE_KEY
+                ).toString(cryptojs.enc.Utf8)
+            ).userId;
+            this.getLikesOfEachPosts(postsWithoutLikes, userId, connection)
+                .then((posts) => {
+                    res.status(200).json({ posts });
+                })
+                .catch((err) => {
+                    res.status(500).json({ error: "Un problème est survenu" });
+                });
+            })
+            .catch((err) => {
+                res.status(500).json({ error: "Un problème est survenu" });
+            });
+        }
+    });
+};
+  
+// Récupération de plusieurs posts
+exports.getSomePosts = (req, res, next) => {
+    const connection = database.connect();
+    const limit = parseInt(req.params.limit);
+    const offset = parseInt(req.params.offset);
+    const sql =
+        "SELECT Posts.id AS postId, Posts.publication_date AS postDate, Posts.imageurl AS postImage, Posts.content as postContent, Users.id AS userId, Users.name AS userName, Users.pictureurl AS userPicture\
+        FROM Posts\
+        INNER JOIN Users ON Posts.user_id = Users.id\
+        ORDER BY postDate DESC\
+        LIMIT ? OFFSET ?;";
+    const sqlParams = [limit, offset];
+    connection.execute(sql, sqlParams, (error, rawPosts, fields) => {
+        if (error) {
+            connection.end();
+            res.status(500).json({ error: error.sqlMessage });
+        } else {
+            this.getCommentsOfEachPosts(rawPosts, connection).then(
+            (postsWithoutLikes) => {
+            const cryptedCookie = new Cookies(req, res).get("snToken");
+            const userId = JSON.parse(
+                cryptojs.AES.decrypt(
+                    cryptedCookie,
+                    process.env.COOKIE_KEY
+                ).toString(cryptojs.enc.Utf8)
+            ).userId;
+            this.getLikesOfEachPosts(postsWithoutLikes, userId, connection).then(
+                (posts) => {
+                    res.status(200).json({ posts });
+                }
+            );
+            }
+        );
+        }
+    });
+};
+
+// Récupération d'un post
+exports.getOnePost = (req, res, next) => {
+    const connection = database.connect();
+    const postId = parseInt(req.params.id);
+    const sql =
+        "SELECT Posts.id AS postId, Posts.publication_date AS postDate, Posts.imageurl AS postImage, Posts.content as postContent, Users.id AS userId, Users.name AS userName, Users.pictureurl AS userPicture\
+        FROM Posts\
+        INNER JOIN Users ON Posts.user_id = Users.id\
+        WHERE Posts.id = ?\
+        ORDER BY postDate DESC";
+    const sqlParams = [postId];
+    connection.execute(sql, sqlParams, (error, rawPosts, fields) => {
+        if (error) {
+            connection.end();
+            res.status(500).json({ error: error.sqlMessage });
+        } else {
+        this.getCommentsOfEachPosts(rawPosts, connection).then(
+            (postsWithoutLikes) => {
+                const cryptedCookie = new Cookies(req, res).get("snToken");
+                const userId = JSON.parse(
+                    cryptojs.AES.decrypt(
+                    cryptedCookie,
+                    process.env.COOKIE_KEY
+                ).toString(cryptojs.enc.Utf8)
+                ).userId;
+                this.getLikesOfEachPosts(postsWithoutLikes, userId, connection).then(
+                (post) => {
+                    res.status(200).json({ post });
+                }
+            );
+            }
+            );
+        }
+    });
+};
+
+// Suppression d'un post
+exports.deletePost = (req, res, next) => {
+    const connection = database.connect();
+    const postId = parseInt(req.params.id, 10);
+    const sql = "DELETE FROM Posts WHERE id=?;";
+    const sqlParams = [postId];
+  
+    connection.query(
+        "SELECT Posts.id AS postId, Posts.publication_date AS postDate, Posts.imageurl AS postImage, Posts.content as postContent, Users.id AS userId, Users.name AS userName, Users.pictureurl AS userPicture\
+        FROM Posts\
+        INNER JOIN Users ON Posts.user_id = Users.id\
+        WHERE Posts.id = ?\
+        ORDER BY postDate DESC",
+        [req.params.id],
+        function (error, results, fields) {
+            if (error) res.status(500).json({ error: error });
+            imageUrl = results[0].postImage;
+            const filename = imageUrl.split("/images/")[1];
+            fs.unlinkSync(`images/${filename}`);
+        }
+    );
+    connection.execute(sql, sqlParams, (error, results, fields) => {
+        if (error) {
+            res.status(500).json({ error: error.sqlMessage });
+        } else {
+            res.status(201).json({ message: "Publication supprimée" });
+        }
+    });
+    connection.end();
+};
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -12,5 +12,9 @@ const multer = require('../middlewares/multer-config');
 
 // Routes
 router.post('/', auth, multer, validate.postContent, postCtrl.newPost);
+router.get('/', auth, postCtrl.getAllPosts);
+router.get('/:id', auth, validate.id, postCtrl.getOnePost);
+router.get('/:limit/:offset', auth, validate.getSomePosts, postCtrl.getSomePosts);
+router.delete('/:id', auth, validate.id, credential.deletePost, postCtrl.deletePost);
 
 module.exports = router;
